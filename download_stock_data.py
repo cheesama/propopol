c698f148a2f982a7668ba0a396d274abbf690fa2
cheesama <cheehoon12@hanmail.net>
2021-02-07 12:04:41 +0900

    modify getting stock data from marcap

---
+++ download_stock_data.py
@@ -0,0 +1,18 @@
+from datetime import datetime, date
+
+import os, sys
+
+def get_all_stock_data():
+    os.system('!git clone "https://github.com/FinanceData/marcap.git" marcap')
+    from marcap import marcap_data
+
+    df = marcap_data('1995-05-02', datetime.today().strftime('%Y-%m-%d'))
+
+    return df
+
+
+
+
+
+
+
